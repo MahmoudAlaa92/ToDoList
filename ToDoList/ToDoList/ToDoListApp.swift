b9af2c1991fc23d1ede5080d4762d64d0dea18c9
MahmoudAlaa92 <mahmoudalaa.wr@gmail.com>
2025-10-30 13:15:57 +0300

    Add Project 🎫

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Mahmoud Alaa on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
